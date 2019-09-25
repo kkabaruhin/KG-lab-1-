73fb4e7368fbe1bb9025afd314fad385d56e671e
Кабарухин <31798826+kkabaruhin@users.noreply.github.com>
2019-09-25 14:13:11 +0300

    Add files via upload

---
+++ lab3/lab3_task2/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace lab3_task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 580);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 606);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выделить границу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.FindBorder);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 605);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Открыть файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OpenFile);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(374, 605);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(374, 637);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(395, 605);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(395, 637);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(483, 606);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(148, 21);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Показать границу";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 676);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Выделение границ связной области";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ lab3/lab3_task2/Form1.cs
@@ -0,0 +1,182 @@
+﻿// Выделение границы связной области
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab3_task2
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp = null;
+        Dictionary<int, SortedSet<int>> dict;
+        Bitmap border = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // для добавления новой пары x, y в dict
+        bool addOrUpdate(int key, int newValue)
+        {
+            bool res = false;
+            SortedSet<int> val;
+            if (dict.TryGetValue(key, out val))
+            {
+                res = dict[key].Add(newValue);
+            }
+            else
+            {
+                dict[key] = new SortedSet<int>();
+                res = dict[key].Add(newValue);
+            }
+            return res;
+        }
+
+        private void FindBorder(object sender, EventArgs e)
+        {
+            // x и y - координаты точки в области, границу которой ищем
+            var x = Convert.ToInt32(textBox1.Text); 
+            var y = Convert.ToInt32(textBox2.Text);
+            var areaColor = bmp.GetPixel(x, y); // цвет области, границу которой ищем
+            dict = new Dictionary<int, SortedSet<int>>();  // y - ключ, иксы упорядочены от меньшего к большему
+            border = new Bitmap(bmp);
+
+            while (bmp.GetPixel(x + 1, y) == areaColor) // ищем первую точку границы
+                x += 1;
+
+            addOrUpdate(y, x + 1);
+            char currentDirection = 'd';
+
+            // стартовые значения x, y и направления 
+            var x_start = x;
+            var y_start = y;
+            var startDirection = 'd';
+
+            if (bmp.GetPixel(x, y + 1) != areaColor)
+                currentDirection = 'l';
+            else
+                y += 1;
+
+            // идем по часовой стрелке, при этом держим границу слева относительно текущего пикселя
+            while (x != x_start || y != y_start || currentDirection != startDirection)
+            {
+                switch (currentDirection){
+                    case 'd':
+                        if (bmp.GetPixel(x + 1, y) != areaColor)
+                        {
+                            addOrUpdate(y, x + 1);
+                            if (bmp.GetPixel(x, y + 1) != areaColor)
+                                currentDirection = 'l';
+                            else
+                                y += 1;
+                        }
+                        else
+                        {
+                            x += 1;
+                            currentDirection = 'r';
+                        }
+                        break;
+
+                    case 'r':
+                        if (bmp.GetPixel(x, y - 1) != areaColor)
+                        {
+                            addOrUpdate(y - 1, x);
+                            if (bmp.GetPixel(x + 1, y) != areaColor)
+                                currentDirection = 'd';
+                            else
+                                x += 1;
+                        }
+                        else
+                        {
+                            y -= 1;
+                            currentDirection = 'u';
+                        }
+                        break;
+
+                    case 'u':
+                        if (bmp.GetPixel(x - 1, y) != areaColor)
+                        {
+                            addOrUpdate(y, x - 1);
+                            if (bmp.GetPixel(x, y - 1) != areaColor)
+                                currentDirection = 'r';
+                            else
+                                y -= 1;
+                        }
+                        else
+                        {
+                            x -= 1;
+                            currentDirection = 'l';
+                        }
+                        break;
+
+                    case 'l':
+                        if (bmp.GetPixel(x, y + 1) != areaColor)
+                        {
+                            addOrUpdate(y + 1, x);
+                            if (bmp.GetPixel(x - 1, y) != areaColor)
+                                currentDirection = 'u';
+                            else
+                                x -= 1;
+                        }
+                        else
+                        {
+                            y += 1;
+                            currentDirection = 'd';
+                        }
+                        break;
+                }
+
+            }
+
+            // строим границу по точкам
+            foreach (KeyValuePair<int, SortedSet<int>> y_entry in dict)
+            {
+                foreach (var x_entry in y_entry.Value)
+                {
+                    border.SetPixel(x_entry, y_entry.Key, Color.Red);
+                }
+            }
+            pictureBox1.Image = border;
+            checkBox1.Checked = true;
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                    bmp = new Bitmap(ofd.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно открыть выбранный файл", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // координаты стартовой точки области
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point coordinates = me.Location;
+            textBox1.Text = coordinates.X.ToString();
+            textBox2.Text = coordinates.Y.ToString();
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                pictureBox1.Image = border;
+            else
+                pictureBox1.Image = bmp;
+        }
+    }
+}
